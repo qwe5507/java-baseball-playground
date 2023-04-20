a05177f465e6dc2042e6e5c8a71b99b782382ef8
qwe5507 <apvmffkdls@gmail.com>
2023-04-20 22:47:10 +0900

    단위 테스트 실습 - 문자열 계산기

---
+++ src/test/java/study/CalcTest.java
@@ -0,0 +1,15 @@
+package study;
+
+import org.junit.jupiter.api.Test;
+
+/**
+ * 단위 테스트 실습 - 문자열 계산기
+ */
+public class CalcTest {
+    @Test
+    void test() {
+        // given
+        // when
+        // then
+    }
+}
